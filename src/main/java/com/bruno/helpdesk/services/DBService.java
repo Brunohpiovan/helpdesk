eca27a3027447ce60c70f0a93bd23bd8bc4a9dc6
Bruno piovan <bpiovan90@gmail.com>
2024-10-03 14:59:58 -0300

    Excecao para email e cpf cadastrados

--- src/main/java/com/bruno/helpdesk/services/DBService.java
+++ src/main/java/com/bruno/helpdesk/services/DBService.java
@@ -39,10 +39,14 @@ public class DBService {
         Cliente cli1 = new Cliente(null,"Linos torvo","667.367.440-75","linos@mail.com","654321");
 
         Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"Chamado 01","Primeiro chamado",tec1,cli1);
+        Chamado ch2 = new Chamado(null, Prioridade.ALTA, Status.ENCERRADO,"Chamado 02","Segundo chamado",tec1,cli1);
+        Chamado ch3 = new Chamado(null, Prioridade.ALTA, Status.ABERTO,"Chamado 03","Terceiro chamado",tec1,cli1);
+        Chamado ch4 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO,"Chamado 04","Quarto chamado",tec1,cli1);
+        Chamado ch5 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"Chamado 05","Quinto chamado",tec1,cli1);
 
         tecnicoRepository.saveAll(Arrays.asList(tec1,tec2,tec3));
         clienteRepository.saveAll(Arrays.asList(cli1));
-        chamadoRepository.saveAll(Arrays.asList(ch1));
+        chamadoRepository.saveAll(Arrays.asList(ch1,ch2,ch3,ch4,ch5));
 
     }
 
--- src/main/java/com/bruno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/bruno/helpdesk/services/TecnicoService.java
@@ -1,8 +1,11 @@
 package com.bruno.helpdesk.services;
 
+import com.bruno.helpdesk.domain.Pessoa;
 import com.bruno.helpdesk.domain.Tecnico;
 import com.bruno.helpdesk.domain.dtos.TecnicoDTO;
+import com.bruno.helpdesk.repositories.PessoaRepository;
 import com.bruno.helpdesk.repositories.TecnicoRepository;
+import com.bruno.helpdesk.services.exceptions.DataIntegrityViolationException;
 import com.bruno.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +18,9 @@ public class TecnicoService {
     @Autowired
     private TecnicoRepository repository;
 
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao encontrado! Id: "+ id));
@@ -26,7 +32,20 @@ public class TecnicoService {
 
     public Tecnico create(TecnicoDTO objDTO) {
         objDTO.setId(null);
+        validaPorCpfeEmail(objDTO);
         Tecnico newObj = new Tecnico(objDTO);
         return repository.save(newObj);
+    }
+
+    private void validaPorCpfeEmail(TecnicoDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw new DataIntegrityViolationException("CPF ja cadastrado no sitema.");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw new DataIntegrityViolationException("E-mail ja cadastrado no sitema.");
+        }
     }
 }
