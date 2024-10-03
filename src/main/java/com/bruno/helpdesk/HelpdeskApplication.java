9826422f6207230789e59768357dea95c37e6537
Bruno piovan <bpiovan90@gmail.com>
2024-10-03 10:53:01 -0300

    Perfil de teste criado

--- src/main/java/com/bruno/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/bruno/helpdesk/HelpdeskApplication.java
@@ -1,53 +1,14 @@
 package com.bruno.helpdesk;
 
-import com.bruno.helpdesk.domain.Chamado;
-import com.bruno.helpdesk.domain.Cliente;
-import com.bruno.helpdesk.domain.Tecnico;
-import com.bruno.helpdesk.domain.enums.Perfil;
-import com.bruno.helpdesk.domain.enums.Prioridade;
-import com.bruno.helpdesk.domain.enums.Status;
-import com.bruno.helpdesk.repositories.ChamadoRepository;
-import com.bruno.helpdesk.repositories.ClienteRepository;
-import com.bruno.helpdesk.repositories.TecnicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-
-	@Autowired
-	private TecnicoRepository tecnicoRepository;
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-
-	@Autowired
-	private ChamadoRepository chamadoRepository;
-
-
+public class HelpdeskApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-
-		Tecnico tec1 = new Tecnico(null,"Valdir cezar","545.974.290-00","Valdir@gmail.com","123456");
-		tec1.addPerfil(Perfil.ADMIN);
-
-		Cliente cli1 = new Cliente(null,"Linos torvo","667.367.440-75","linos@mail.com","654321");
-
-		Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"Chamado 01","Primeiro chamado",tec1,cli1);
-
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(ch1));
-
-
-	}
 }
+
---
+++ src/main/java/com/bruno/helpdesk/services/DBService.java
@@ -0,0 +1,45 @@
+package com.bruno.helpdesk.services;
+
+import com.bruno.helpdesk.domain.Chamado;
+import com.bruno.helpdesk.domain.Cliente;
+import com.bruno.helpdesk.domain.Tecnico;
+import com.bruno.helpdesk.domain.enums.Perfil;
+import com.bruno.helpdesk.domain.enums.Prioridade;
+import com.bruno.helpdesk.domain.enums.Status;
+import com.bruno.helpdesk.repositories.ChamadoRepository;
+import com.bruno.helpdesk.repositories.ClienteRepository;
+import com.bruno.helpdesk.repositories.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    @Bean
+    public void instanciaDB(){
+        Tecnico tec1 = new Tecnico(null,"Valdir cezar","545.974.290-00","Valdir@gmail.com","123456");
+        tec1.addPerfil(Perfil.ADMIN);
+
+        Cliente cli1 = new Cliente(null,"Linos torvo","667.367.440-75","linos@mail.com","654321");
+
+        Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"Chamado 01","Primeiro chamado",tec1,cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        chamadoRepository.saveAll(Arrays.asList(ch1));
+
+    }
+
+}
