9dc69129acf042c0b73de5968bc7384d73389208
Bruno piovan <bpiovan90@gmail.com>
2024-10-03 13:46:36 -0300

    findAll para tecnicos

--- src/main/java/com/bruno/helpdesk/services/DBService.java
+++ src/main/java/com/bruno/helpdesk/services/DBService.java
@@ -31,12 +31,16 @@ public class DBService {
     public void instanciaDB(){
         Tecnico tec1 = new Tecnico(null,"Valdir cezar","545.974.290-00","Valdir@gmail.com","123456");
         tec1.addPerfil(Perfil.ADMIN);
+        Tecnico tec2 = new Tecnico(null,"Pedro silva","682.025.280-38","Pedro@gmail.com","1q2w3e");
+        tec1.addPerfil(Perfil.ADMIN);
+        Tecnico tec3 = new Tecnico(null,"Luan souza","620.137.020-00","Luan@gmail.com","918201");
+        tec1.addPerfil(Perfil.ADMIN);
 
         Cliente cli1 = new Cliente(null,"Linos torvo","667.367.440-75","linos@mail.com","654321");
 
         Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"Chamado 01","Primeiro chamado",tec1,cli1);
 
-        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        tecnicoRepository.saveAll(Arrays.asList(tec1,tec2,tec3));
         clienteRepository.saveAll(Arrays.asList(cli1));
         chamadoRepository.saveAll(Arrays.asList(ch1));
 
